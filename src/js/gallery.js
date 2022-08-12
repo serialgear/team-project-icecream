3225de5b99c5327da8fa1c62575edd123f816a58
Mich47 <mmitya889@gmail.com>
2022-08-12 12:59:18 +0300

    add gallery for tab, desk

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -5,20 +5,20 @@ window.onload = () => {
 
   const masonry = new Masonry(grid, {
     itemSelector: '.grid-item',
-
+    stamp: '.stamp',
     gutter: 2,
     stagger: 30,
     // horizontalOrder: true,
     columnWidth: '.grid-sizer',
-    percentPosition: true,
+    percentPosition: false,
   });
 
   if (window.matchMedia('(min-width: 1200px)').matches) {
-    masonry = new Masonry(grid, {
+    const masonry = new Masonry(grid, {
       gutter: 8,
     });
   } else if (window.matchMedia('(min-width: 768px)').matches) {
-    masonry = new Masonry(grid, {
+    const masonry = new Masonry(grid, {
       gutter: 6,
     });
   }
