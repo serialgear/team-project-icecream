0a78e500047065386717735b895d06411015da04
AntonSmishko <anton.smishko2014@gmail.com>
2022-08-15 22:10:35 +0300

    add-product-modal-style

---
+++ src/js/products-modal.js
@@ -0,0 +1,19 @@
+var modal = document.getElementById('myModal');
+var btn = document.getElementById('myBtn')
+
+var modalClose = document.getElementsByClassName("products-modal")[0]
+
+btn.onclick = function () {
+    modal.style.display = "block";
+
+}
+
+btn.onclick = function () {
+    modal.style.display = "none";
+
+}
+
+ window.onclick = function (event) {
+     if (event.target == modal){modal.style.display = "none"}
+    
+ }
