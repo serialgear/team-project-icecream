3f08e68c19de65cc992cdbc2fb8d3cd2443f2052
Iliev Vlad <ilievvlad26@gmail.com>
2022-08-16 02:51:00 +0300

    fix header-hero

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ import './js/slick';
 import './js/gallery';
 import './js/menu';
 import './js/modal-jquery';
+import './js/lazy-scroll';
+import './js/function';
--- src/js/menu.js
+++ src/js/menu.js
@@ -3,12 +3,18 @@
 		openMobileMenuBtn: document.querySelector("[data-mobile-menu-open]"),
 		closeMobileMenuBtn: document.querySelector("[data-mobile-menu-close]"),
 		mobileMenu: document.querySelector("[data-mobile-menu]"),
+		hiddenElementBurger: document.querySelector("[data-hidden-burger]"),
+		hiddenElementButton: document.querySelector("[data-hidden-button]"),
+		menuList: document.querySelector(".mob-menu__list")
 	};
 
 	refs.openMobileMenuBtn.addEventListener("click", toggleMobileMenu);
 	refs.closeMobileMenuBtn.addEventListener("click", toggleMobileMenu);
+	refs.menuList.addEventListener("click", toggleMobileMenu);
 
 	function toggleMobileMenu() {
 		refs.mobileMenu.classList.toggle("is-hidden");
+		refs.hiddenElementBurger.classList.toggle("is-hidden");
+		refs.hiddenElementButton.classList.toggle("is-hidden");
 	}
 })();
