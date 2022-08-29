d4f564dbc3879e5354cef04c9c077988ac9e705f
serialgear <ukr.tretyakov@gmail.com>
2022-08-30 00:49:23 +0300

    Create test.js

---
+++ src/js/test.js
@@ -0,0 +1,9 @@
+function getExtremeElements(array) {
+  // Change code below this line
+  return array.slice(1, array.length - 1);
+
+  // Change code above this line
+}
+getExtremeElements([1, 2, 3, 4, 5]);
+
+console.log(array);
